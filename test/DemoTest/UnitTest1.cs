7180e71b515d59eb08d7e8ea2730efa70e8abc2c
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-27 12:01:42 +0200

    Add project files.

---
+++ test/DemoTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using DemoAPI.Controllers;
+
+namespace DemoTest
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
